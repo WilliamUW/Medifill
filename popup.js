c7d5576bc837e4c325e8851e3a874bb2929dd0c8
William Wang <william.wang1@uwaterloo.ca>
2023-02-12 08:38:59 -0500

    Working fields

--- popup.js
+++ popup.js
@@ -1,10 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
 	chrome.tabs.executeScript(null, { code: "document.getElementById('first-name').value = '" + "William" + "'" });
-	chrome.tabs.executeScript(null, { code: "document.getElementById('input100').value = '" + "Wang" + "'" });
+	chrome.tabs.executeScript(null, { code: "document.getElementById('last-name').value = '" + "Wang" + "'" });
 	chrome.tabs.executeScript(null, { code: "document.getElementById('email').value = '" + "williamwang@gmail.com" + "'" });
 	chrome.tabs.executeScript(null, { code: "document.getElementById('message').value = '" + "SIN: 123456789" + "'" });
 
 
 	chrome.tabs.executeScript(null, { code: "document.getElementById('phone').value = '" + "134" + "'" });
+
+	chrome.tabs.executeScript(null, { code: "document.getElementById('drivers').value = '" + "W8374-19819-46214" + "'" });
+	chrome.tabs.executeScript(null, { code: "document.getElementById('sin').value = '" + "123456789" + "'" });
+	chrome.tabs.executeScript(null, { code: "document.getElementById('insurance').value = '" + "1EG4-TE5-MK72" + "'" });
 
 });
