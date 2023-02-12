2583c0c1f00e99a55b7a769986a68863bb3cecfb
William Wang <william.wang1@uwaterloo.ca>
2023-02-12 08:40:30 -0500

    Insurance

--- popup.js
+++ popup.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
 	chrome.tabs.executeScript(null, { code: "document.getElementById('first-name').value = '" + "William" + "'" });
 	chrome.tabs.executeScript(null, { code: "document.getElementById('last-name').value = '" + "Wang" + "'" });
 	chrome.tabs.executeScript(null, { code: "document.getElementById('email').value = '" + "williamwang@gmail.com" + "'" });
-	chrome.tabs.executeScript(null, { code: "document.getElementById('message').value = '" + "SIN: 123456789" + "'" });
+	chrome.tabs.executeScript(null, { code: "document.getElementById('message').value = '" + "Symptoms: Allergic to Bunnies" + "'" });
 
 
 	chrome.tabs.executeScript(null, { code: "document.getElementById('phone').value = '" + "134" + "'" });
